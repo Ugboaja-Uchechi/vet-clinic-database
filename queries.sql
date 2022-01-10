6f7af6b6f1294ecd82ce63d16f7097db5a57d9d9
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-10 18:37:47 +0100

    Fix code review in query file

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals WHERE name LIKE '%mon%';
+SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered='true' AND escape_attempt < 3;
 SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
