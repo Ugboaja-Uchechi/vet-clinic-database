8293b54e986eac2f002411145268a1889b82d0c1
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-11 17:34:03 +0100

    Add neccessary comments to make the file more understandable

--- data.sql
+++ data.sql
@@ -12,26 +12,45 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
 
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction.
 BEGIN;
 UPDATE animals SET species = 'unspecified'
 ROLLBACK;
 
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
-COMMIT;
 
-BEGIN;
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
 UPDATE animals SET species = 'pokemon' WHERE species = null;
+
+-- COMMIT THE TRANSACTION
 COMMIT;
 
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
 
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
+
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-22';
+
+-- CREATE A SAVE POINT FOR THE TRANSACTION
 SAVEPOINT first_delete;
+
+-- Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- ROLLBACK TO THE SAVEPOINT
 ROLLBACK TO first_delete;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- COMMIT TRANSACTION
 COMMIT;
--- queries.sql
+++ queries.sql
@@ -9,9 +9,20 @@ SELECT * FROM animals WHERE neutered='true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- How many animals are there?
 SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
 SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts <= 0;
+
+-- What is the average weight of animals?
 SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
 SELECT neutered, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+-- What is the minimum and maximum weight of each type of animal?
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
