8a3225e95bebc1a6b4f992c6981f8bfd2cd06d81
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-11 18:32:17 +0100

    Add aueries to the right file

--- queries.sql
+++ queries.sql
@@ -9,6 +9,49 @@ SELECT * FROM animals WHERE neutered='true';
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction.
+BEGIN;
+UPDATE animals SET species = 'unspecified'
+ROLLBACK;
+
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+
+UPDATE animals SET species = 'pokemon' WHERE species = null;
+
+-- COMMIT THE TRANSACTION
+COMMIT;
+
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-22';
+
+-- CREATE A SAVE POINT FOR THE TRANSACTION
+SAVEPOINT first_delete;
+
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- ROLLBACK TO THE SAVEPOINT
+ROLLBACK TO first_delete;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- COMMIT TRANSACTION
+COMMIT;
+
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
 
