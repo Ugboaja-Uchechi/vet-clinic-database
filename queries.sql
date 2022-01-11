b509ae23b0063e5cdce489de50df7bcf2aaea16c
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-11 18:57:00 +0100

    Show verification

--- queries.sql
+++ queries.sql
@@ -26,10 +26,42 @@ UPDATE animals SET species = 'pokemon' WHERE species = null;
 -- COMMIT THE TRANSACTION
 COMMIT;
 
+-- Verify that change was made and persists after commit.
+
+--  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species
+-- ----+------------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |       8.0 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |      11.0 | digimon
+--   6 | Plantmon   | 2022-11-15    |               2 | t        |      -5.7 | digimon
+--   8 | Angemon    | 2005-06-12    |               1 | t        |     -45.0 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--   5 | Charmander | 2020-02-08    |               0 | f        |     -11.0 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |      17.0 | pokemon
+-- (10 rows)
+
 -- Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
+
+--After the roll back verify if all records in the animals table still exist.
+
+--  id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species
+-- ----+------------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |       8.0 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |      11.0 | digimon
+--   6 | Plantmon   | 2022-11-15    |               2 | t        |      -5.7 | digimon
+--   8 | Angemon    | 2005-06-12    |               1 | t        |     -45.0 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--   5 | Charmander | 2020-02-08    |               0 | f        |     -11.0 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |    -12.13 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |      17.0 | pokemon
+-- (10 rows)
 
 -- Inside a transaction:
 -- Delete all animals born after Jan 1st, 2022.
@@ -51,6 +83,23 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 
 -- COMMIT TRANSACTION
 COMMIT;
+
+-- Verify that change was made and persists after commit.
+
+-- id |    name    | date_of_birth | escape_attempts | neutered | weight_kg | species
+-- ----+------------+---------------+-----------------+----------+-----------+---------
+--   1 | Agumon     | 2020-02-03    |               0 | t        |     10.23 | digimon
+--   2 | Gabumon    | 2018-11-15    |               2 | t        |       8.0 | digimon
+--   4 | Devimon    | 2017-05-12    |               5 | t        |      11.0 | digimon
+--   9 | Boarmon    | 2005-06-07    |               7 | t        |      20.4 | digimon
+--   3 | Pikachu    | 2021-01-07    |               1 | f        |     15.04 | pokemon
+--  10 | Blossom    | 1998-10-13    |               3 | t        |      17.0 | pokemon
+--   8 | Angemon    | 2005-06-12    |               1 | t        |      45.0 | digimon
+--   5 | Charmander | 2020-02-08    |               0 | f        |      11.0 | pokemon
+--   7 | Squirtle   | 1993-04-02    |               3 | f        |     12.13 | pokemon
+-- (9 rows)
+
+
 
 -- How many animals are there?
 SELECT COUNT(*) FROM animals;
