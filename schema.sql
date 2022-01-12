532ea7b962d111928ff3e12e9427f84bf897a017
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-01-12 14:33:39 +0100

    Alter and create new tables in schema file

--- schema.sql
+++ schema.sql
@@ -11,3 +11,29 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(100);
+
+CREATE TABLE owners(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(100),
+    age INT
+);
+
+CREATE TABLE species(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100)
+);
+
+ALTER TABLE animals
+ADD PRIMARY KEY (id)
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
